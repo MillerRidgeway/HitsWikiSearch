b81d23db6e780f12115d486177447288697c4be6
Miller Ridgeway <miller.ridgeway@gmail.com>
2020-02-02 15:48:00 -0700

    Got datasets loading in to DF, can start manipulating now

--- src/main/scala/WikiSearch.scala
+++ src/main/scala/WikiSearch.scala
@@ -1,12 +1,22 @@
 import org.apache.spark.sql.SparkSession
 
+
+case class Title(id: Int, name: String)
 object WikiSearch {
   def main(args: Array[String]) {
+    //Files
     val titlesFile = "hdfs://richmond:32251/user/millerr/titles-sorted.txt"
     val linksFile = "hdfs://richmond:32251/user/millerr/links-simple-sorted.txt"
+
+    //Spark Session
     val spark = SparkSession.builder.appName("WikiSearch").getOrCreate()
 
-    println("Hello, World!")
+    val titlesDf = spark.read.textFile(titlesFile).rdd.zipWithIndex()
+    val linksDf = spark.read.textFile(linksFile)
+
+    println(titlesDf.first())
+    println(linksDf.first())
+    
     spark.stop()
   }
 }
