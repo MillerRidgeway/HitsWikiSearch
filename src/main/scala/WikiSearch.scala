da4aa8d2bdb835aec92201408702b31b6a4e3bd0
Miller Ridgeway <miller.ridgeway@gmail.com>
2020-02-03 15:54:06 -0700

    Making sure everything is in a dataframe is probably a good idea - that's what I am working on

--- src/main/scala/WikiSearch.scala
+++ src/main/scala/WikiSearch.scala
@@ -1,23 +1,33 @@
 import org.apache.spark.sql.SparkSession
+
+case class TitleRecord(name: String, id: Long)
+
 object WikiSearch {
   def main(args: Array[String]) {
     //Files
     val titlesFile = "hdfs://richmond:32251/user/millerr/titles-sorted.txt"
     val linksFile = "hdfs://richmond:32251/user/millerr/links-simple-sorted.txt"
 
+    //Query
+    val query = "Colorado_State_University"
+
     //Spark Session
     val spark = SparkSession.builder.appName("WikiSearch").getOrCreate()
 
     //Read in files as dataframes
-    val titlesDf = spark.read.textFile(titlesFile).rdd.zipWithIndex()
-    val linksDf = spark.read.textFile(linksFile)
+    val titlesRdd = spark.read.textFile(titlesFile).rdd.zipWithIndex()
+    val linksDf = spark.read.option("delimiter", ":").csv(linksFile).toDF("from", "to")
+
+
 
     //titlesDf.take(10).foreach(println)
     //linksDf.take(10).foreach(println)
 
     //Root set generation
-    val query = "Colorado_State_University"
-    val rootSet = titlesDf.filter(s => s._1.contains(query))
+    val rootSet = titlesRdd.filter(s => s._1.contains(query))
+
+    //Base set generation
+    val baseSet =
 
     rootSet.take(10).foreach(println)
 
