05427349da4c97ea913cf9d5301906501baa1cf3
Miller Ridgeway <miller.ridgeway@gmail.com>
2020-02-16 23:11:05 -0700

    Can't quite get hubs working yet, need to figure out a solution

--- src/main/scala/WikiSearch.scala
+++ src/main/scala/WikiSearch.scala
@@ -1,5 +1,5 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{lit,udf}
 import org.apache.spark.sql
 
 object WikiSearch {
@@ -13,7 +13,7 @@ object WikiSearch {
     val linksFile = "hdfs://richmond:32251/user/millerr/links-simple-sorted.txt"
 
     //Search string
-    val query = "Rocky_Mountain_National_Park"
+    val query ="Rocky_Mountain_National_Park" //"eclipse"
 
     //Read in files - links as dataframe, titles as RDD (later converted)
     val titlesRdd = spark.read.textFile(titlesFile).rdd.zipWithIndex()
@@ -30,15 +30,16 @@ object WikiSearch {
     var baseSet = rootSet.join(linksDf, $"id" === $"from" || linksDf("to").contains($"id"))
       .withColumn("AuthScore", lit(1L))
       .withColumn("HubScore", lit(1L))
-      .withColumnRenamed("id","root_set_id")
+      .drop("id")
       .withColumn("from", $"from".cast(sql.types.LongType))
+      .distinct()
     baseSet.show(10)
     baseSet.printSchema()
     baseSet.persist()
 
     //Iterate and calculate Hub/Authority Score
 
-    //Grab authority scores using the 'to' column
+    //Calc auth scores
     val auths = baseSet.rdd.flatMap(row =>
       row.getAs[String]("to").split(" ").map(item =>
         (item,row.getAs[Long]("HubScore"))))
@@ -56,17 +57,18 @@ object WikiSearch {
     baseSet = baseSet.drop("AuthScore").join(normalizedAuths, Seq("from"))
     baseSet.show(10)
 
-//    val hubs = baseSet.rdd.flatMap(row =>
-//      row.getAs[String]("to").split(" ").map(item =>
-//        (row.getAs[Long]("from"),
-//          normalizedAuths.filter(x => x.getAs[Long]("from") == item.toLong).select("AuthScore")
-//            .take(1)(0).getAs[Long]("AuthScore"))))
-//        .reduceByKey((a,b) => a + b)
-//        .toDF("from", "HubScore")
-//    hubs.show(10)
+    val hubs = baseSet.rdd.flatMap(row =>
+      row.getAs[String]("to").split(" ").map(item =>
+        (row.getAs[Long]("from"),
+          if(baseSet.filter($"from" === item).head(1).isEmpty) 0
+          else baseSet.filter($"from" === item).first().getAs[Long]("AuthScore"))))
+      .reduceByKey((a,b) => a + b)
+      .toDF("from", "HubScore")
+    hubs.persist()
+    hubs.show(10)
+
     //baseSet.sort($"AuthScore".desc).limit(10).show()
 
     spark.stop()
   }
-
 }
