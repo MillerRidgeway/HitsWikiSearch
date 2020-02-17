a4bb71fa1f683514d0055ab8d7555c4f48d47eac
Miller Ridgeway <miller.ridgeway@gmail.com>
2020-02-17 15:53:42 -0700

    Got hub score calc working, just need to fix the types

--- src/main/scala/WikiSearch.scala
+++ src/main/scala/WikiSearch.scala
@@ -30,17 +30,17 @@ object WikiSearch {
     var baseSet = rootSet.join(linksDf, $"id" === $"from" || linksDf("to").contains($"id"))
       .withColumn("AuthScore", lit(1L))
       .withColumn("HubScore", lit(1L))
-      .drop("id")
       .withColumn("from", $"from".cast(sql.types.LongType))
+      .drop("id")
       .distinct()
     baseSet.show(10)
     baseSet.printSchema()
     baseSet.persist()
 
     //Iterate and calculate Hub/Authority Score
-
+    //-------Iterate here------
     //Calc auth scores
-    val auths = baseSet.rdd.flatMap(row =>
+    var auths = baseSet.rdd.flatMap(row =>
       row.getAs[String]("to").split(" ").map(item =>
         (item,row.getAs[Long]("HubScore"))))
       .reduceByKey((a,b) => a + b)
@@ -48,8 +48,8 @@ object WikiSearch {
     auths.persist()
 
     //Normalize the auth scores
-    val authsSum = auths.select("AuthScore").rdd.map(row => row.getAs[Long]("AuthScore")).reduce(_+_)
-    val normalizedAuths = auths.withColumn("AuthScore", $"AuthScore"/authsSum)
+    var authsSum = auths.select("AuthScore").rdd.map(row => row.getAs[Long]("AuthScore")).reduce(_+_)
+    var normalizedAuths = auths.withColumn("AuthScore", $"AuthScore"/authsSum)
     normalizedAuths.show(10)
     normalizedAuths.persist()
 
@@ -57,15 +57,21 @@ object WikiSearch {
     baseSet = baseSet.drop("AuthScore").join(normalizedAuths, Seq("from"))
     baseSet.show(10)
 
-    val hubs = baseSet.rdd.flatMap(row =>
-      row.getAs[String]("to").split(" ").map(item =>
-        (row.getAs[Long]("from"),
-          if(baseSet.filter($"from" === item).head(1).isEmpty) 0
-          else baseSet.filter($"from" === item).first().getAs[Long]("AuthScore"))))
-      .reduceByKey((a,b) => a + b)
-      .toDF("from", "HubScore")
+    var hubs = baseSet.rdd.flatMap(row =>row.getAs[String]("to").split(" ")
+      .map(item => (item, row.getAs[Long]("from"))))
+      .toDF("dest", "source")
+      .join(normalizedAuths, $"from" === $"dest")
+      .drop("from")
+      .groupBy($"source").sum("AuthScore")
+      .withColumnRenamed("sum(AuthScore)", "HubScore")
+      //.withColumn("HubScore",$"HubScore".cast(sql.types.LongType))
     hubs.persist()
+    hubs.printSchema()
     hubs.show(10)
+
+//    var hubsSum = hubs.select("HubScore").rdd.map(row => row.getAs[Long]("HubScore")).reduce(_+_)
+//    var normalizedHubs = hubs.withColumn("HubScore", $"HubScore"/hubsSum)
+//    normalizedHubs.show(10)
 
     //baseSet.sort($"AuthScore".desc).limit(10).show()
 
